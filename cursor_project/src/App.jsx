cddddd27c3bd1ee87151cad46e880e9bba27ad13
sugyeongsu <78544372+susoro@users.noreply.github.com>
2025-05-11 16:22:22 +0900

    로컬 화면 생성 완료
    http://localhost:5173/

--- cursor_project/src/App.jsx
+++ cursor_project/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          여기가 메인이에요
         </p>
       </div>
       <p className="read-the-docs">
